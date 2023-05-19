f952309bc1e9c5efac93e97e5cd8e096c18d0a71
Luis Gomero <lgomero94@gmail.com>
2023-05-18 23:42:38 -0500

    Adding new version of Guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import {  CanActivate } from '@angular/router';
+import { CanActivate } from '@angular/router';
 
 import { TokenService } from '@services/token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(
-    private tokenService: TokenService,
-    private router: Router
-  ){}
+  constructor(private tokenService: TokenService, private router: Router) {}
 
   canActivate(): boolean {
     const isValidToken = this.tokenService.isValidRefreshToken();
@@ -23,5 +19,16 @@ export class AuthGuard implements CanActivate {
     }
     return true;
   }
-
 }
+
+// Angular v15 update
+// export const authGuard: CanActivateFn = () => {
+//   const token = inject(TokenService).getToken();
+//   const router = inject(Router);
+
+//   if (!token) {
+//   router.navigate([’/login’]);
+//   return false;
+//   }
+//   return true;
+//   };
--- src/app/guards/redirect.guard.ts
+++ src/app/guards/redirect.guard.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import {  CanActivate } from '@angular/router';
+import { CanActivate } from '@angular/router';
 
 import { TokenService } from '@services/token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RedirectGuard implements CanActivate {
-
-  constructor(
-    private tokenService: TokenService,
-    private router: Router
-  ){}
+  constructor(private tokenService: TokenService, private router: Router) {}
 
   canActivate(): boolean {
     const isValidToken = this.tokenService.isValidRefreshToken();
@@ -21,5 +17,14 @@ export class RedirectGuard implements CanActivate {
     }
     return true;
   }
-
 }
+
+// export const redirectGuard: CanActivateFn = () => {
+//   const token = inject(TokenService).getToken();
+//   const router = inject(Router);
+
+//   if (token) {
+//     router.navigate(['/app']);
+//   }
+//   return true;
+// };
