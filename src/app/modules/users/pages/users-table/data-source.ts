5dd58e55e524aa0defe0baa1fa2ef68362d84326
Luis Gomero <lgomero94@gmail.com>
2023-05-19 00:26:16 -0500

    Adding Generic Datasource

--- src/app/modules/users/pages/users-table/data-source.ts
+++ src/app/modules/users/pages/users-table/data-source.ts
@@ -4,9 +4,8 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '@models/user.model';
 
 export class DataSourceUser extends DataSource<User> {
-
   data = new BehaviorSubject<User[]>([]);
-  originalData: User[]= [];
+  originalData: User[] = [];
 
   connect(): Observable<User[]> {
     return this.data;
@@ -17,6 +16,23 @@ export class DataSourceUser extends DataSource<User> {
     this.data.next(data);
   }
 
-  disconnect() { }
-
+  disconnect() {}
 }
+
+// export class GenericDataSource<T> extends DataSource<T> {
+
+//   data = new BehaviorSubject<T[]>([]);
+//   originalData: T[] = [];
+
+//   connect(): Observable<T[]> {
+//     return this.data;
+//   }
+
+//   init(data: T[]) {
+//     this.originalData = data;
+//     this.data.next(data);
+//   }
+
+//   disconnect() {}
+
+// }
